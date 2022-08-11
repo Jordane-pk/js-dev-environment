4c0c85dfe9319a7c52e35c1f0b1fcc8de565c94a
jordanePK <jordane@pkinformation.com>
2022-08-11 13:44:26 -0400

    Initial set up of the chart

--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
-//Here we're importing items we'll need. You can add other imports here.
+import c3 from "c3";
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
 
-//
+const chart = c3.generate({
+  bindto: '#chart',
+  size: {
+    height: 480,
+    width: 800
+  },
+  data: {
+    json: [
+      {
+        "month": "Jan",
+        "Peaches": 327,
+        "Bananas": 437,
+        "Pears": 111
+      },
+      {
+        "month": "Feb",
+        "Peaches": 555,
+        "Bananas": 454,
+        "Pears": 222
+      },
+      {
+        "month": "Mar",
+        "Peaches": 242,
+        "Bananas": 864,
+        "Pears": 343
+      },
+      {
+        "month": "Apr",
+        "Peaches": 222,
+        "Bananas": 454,
+        "Pears": 434
+      }
+    ],
+    keys: {
+      x: "month",
+      value: ["Peaches", "Bananas", "Pears"],
+    },
+
+      type: 'bar', 
+      labels: true,
+
+  },
+  axis: {
+    x: {
+      show: true,
+      type: "category",
+    }, 
+    y: {
+      show: true,
+    }
+  },
+  color: {
+    pattern: ['#e09cf7', '#9edbf7', '#9ef7bd']
+  },
+});
