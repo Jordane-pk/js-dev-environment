6495c11676dd5c99546f68a2d6c3424f96231c90
jordane <jordane@pkinformation.com>
2022-08-11 15:57:34 -0400

    updated code to have it work on laptop

--- src/index.js
+++ src/index.js
@@ -1,58 +1,65 @@
 import c3 from "c3";
 
+window.loadChart = (json) => {
+  console.log("json is: ", json);
 
-const chart = c3.generate({
-  bindto: '#chart',
-  size: {
-    height: 480,
-    width: 800
-  },
-  data: {
-    json: [
-      {
-        "month": "Jan",
-        "Peaches": 327,
-        "Bananas": 437,
-        "Pears": 111
-      },
-      {
-        "month": "Feb",
-        "Peaches": 555,
-        "Bananas": 454,
-        "Pears": 222
-      },
-      {
-        "month": "Mar",
-        "Peaches": 242,
-        "Bananas": 864,
-        "Pears": 343
-      },
-      {
-        "month": "Apr",
-        "Peaches": 222,
-        "Bananas": 454,
-        "Pears": 434
-      }
-    ],
-    keys: {
-      x: "month",
-      value: ["Peaches", "Bananas", "Pears"],
+  const obj = JSON.parse(json);
+  const data = obj.data;
+  const type = obj.type;
+
+  const chart = c3.generate({
+    bindto: "#chart",
+    size: {
+      height: 480,
+      width: 800,
     },
+    data: {
+      json: data,
+      // [
+      //   {
+      //     "month": "Jan",
+      //     "Peaches": 327,
+      //     "Bananas": 437,
+      //     "Pears": 111
+      //   },
+      //   {
+      //     "month": "Feb",
+      //     "Peaches": 555,
+      //     "Bananas": 454,
+      //     "Pears": 222
+      //   },
+      //   {
+      //     "month": "Mar",
+      //     "Peaches": 242,
+      //     "Bananas": 864,
+      //     "Pears": 343
+      //   },
+      //   {
+      //     "month": "Apr",
+      //     "Peaches": 222,
+      //     "Bananas": 454,
+      //     "Pears": 434
+      //   }
+      // ],
+      keys: {
+        x: "month",
+        value: ["Peaches", "Bananas", "Pears"],
+      },
 
-      type: 'bar', 
+      type: type,
       labels: true,
-
-  },
-  axis: {
-    x: {
-      show: true,
-      type: "category",
-    }, 
-    y: {
-      show: true,
-    }
-  },
-  color: {
-    pattern: ['#e09cf7', '#9edbf7', '#9ef7bd']
-  },
-});
+    },
+    axis: {
+      x: {
+        show: true,
+        type: "category",
+      },
+      y: {
+        show: true,
+      },
+    },
+    color: {
+      pattern: ["#e09cf7", "#9edbf7", "#9ef7bd"],
+    },
+  });
+};
